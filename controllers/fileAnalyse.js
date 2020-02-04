b7fc7d79cdee445821571d75d107259aba1d7712
Ally Murray <allymurray88@gmail.com>
2020-02-04 22:03:18 +0000

    Form upload working

--- controllers/fileAnalyse.js
+++ controllers/fileAnalyse.js
@@ -1,7 +1,22 @@
 const fileAnalyseRouter = require('express').Router();
+const multiparty = require('multiparty');
 
-fileAnalyseRouter.get('/', (request, response) => {
-  response.json({});
+fileAnalyseRouter.post('/', (request, response) => {
+  const form = new multiparty.Form();
+
+  form.parse(request, function(error, fields, files) {
+    if (error) {
+      console.log(error);
+      return response.json({ error: 'Invalid upload' });
+    }
+
+    const file = files.upfile[0];
+    response.json({
+      name: file.originalFilename,
+      type: file.headers['content-type'],
+      size: file.size
+    });
+  });
 });
 
 module.exports = fileAnalyseRouter;
